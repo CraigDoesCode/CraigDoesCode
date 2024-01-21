fe383f1e15d4ab3f7bb56d1915e0f874dd810289
Craig Norford <craig.a.norford@gmail.com>
2024-01-21 14:07:44 +0000

    add images and read files

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-// index.js
-const Mustache = require('mustache');
-const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';
+import Mustache from "mustache";
+import * as path from "path";
+import * as fs from "fs";
+const MUSTACHE_MAIN_DIR = "./main.mustache";
+const directory = "./images";
 /**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
+ * DATA is the object that contains all
+ * the data to be provided to Mustache
+ * Notice the "name" and "date" property.
+ */
 let DATA = {
-  name: 'Craig',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
+  name: "Craig",
+  date: new Date().toLocaleDateString("en-GB", {
+    weekday: "long",
   }),
 };
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+const stack = fs.readdir(directory, (err, files) => {
+  let stackItems = [];
+  files.forEach((file) => {
+    // get the details of the file
+    let fileDetails = fs.lstatSync(path.resolve(directory, file));
+    // check if the file is directory
+    if (fileDetails.isDirectory()) {
+      console.log("Directory: " + file);
+    } else {
+      stackItems.push(file);
+    }
+  });
+  console.log(stackItems);
+});
+
+const generateReadMe = () => {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
+    fs.writeFileSync("README.md", output);
   });
-}
+};
 generateReadMe();
